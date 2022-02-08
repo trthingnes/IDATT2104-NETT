a9e413910253f325dd425fb8d764c2907e0736ee
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-02-08 16:30:18 +0100

    Finished O3

--- O3/src/main/kotlin/SocketServer.kt
+++ O3/src/main/kotlin/SocketServer.kt
@@ -3,7 +3,7 @@ import java.net.ServerSocket
 import java.net.Socket
 
 fun main() {
-    SocketServer(1200) {
+    /*SocketServer(1200) {
         val reader = SocketServer.makeReader(it)
         val writer = SocketServer.makeWriter(it)
 
@@ -15,6 +15,48 @@ fun main() {
             line = reader.readLine()
         }
 
+        it.close()
+    }*/
+
+    SocketServer(1200) {
+        val reader = SocketServer.makeReader(it)
+        val writer = SocketServer.makeWriter(it)
+        var running = true
+
+        while(running) {
+            writer.println("Hello, I will do your math! Type your operator:")
+            var line = reader.readLine()
+
+            while (!line.equals("+") && !line.equals("-")) {
+                writer.println("I didn't recognize that, type '+' or '-'.")
+                line = reader.readLine()
+            }
+
+            val plus = line.equals("+")
+
+            val numbers = arrayListOf<Int>()
+            while (numbers.size < 2) {
+                writer.println("Type a number:")
+                line = reader.readLine()
+
+                try {
+                    numbers.add(Integer.parseInt(line))
+                }
+                catch(_: Exception) {/* empty */}
+            }
+
+            writer.println("Thanks! Your result: " +
+                    "${numbers[0]} " +
+                    "${if (plus) "+" else "-"} " +
+                    "${numbers[1]} " +
+                    "= ${ if (plus) (numbers[0] + numbers[1]) else (numbers[0] - numbers[1]) }" +
+                    " | Press enter to quit, type something to continue."
+            )
+            running = reader.readLine() != null
+        }
+
+        reader.close()
+        writer.close()
         it.close()
     }
 }
@@ -34,7 +76,7 @@ class SocketServer(port: Int, handler: (Socket) -> Unit) {
 
     companion object {
         fun makeReader(c: Socket) = BufferedReader(InputStreamReader(c.getInputStream()))
-        fun makeWriter(c: Socket) = PrintWriter(c.getOutputStream(), false)
+        fun makeWriter(c: Socket) = PrintWriter(c.getOutputStream(), true)
     }
 
 }
