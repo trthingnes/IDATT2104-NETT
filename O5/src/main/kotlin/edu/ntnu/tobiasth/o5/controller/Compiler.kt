589fbd59a7d95c2d7dd427758c4a7cc7657f0946
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-02-22 10:08:29 +0100

    Finished O5

--- O5/src/main/kotlin/edu/ntnu/tobiasth/o5/controller/Compiler.kt
+++ O5/src/main/kotlin/edu/ntnu/tobiasth/o5/controller/Compiler.kt
@@ -3,6 +3,7 @@ package edu.ntnu.tobiasth.o5.controller
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -15,6 +16,7 @@ import kotlin.io.path.absolutePathString
 @RestController
 class Compiler {
     @PostMapping("/compile")
+    @CrossOrigin
     fun compile(@RequestBody data: Map<String, Any>): ResponseEntity<Any> {
         val logger = LoggerFactory.getLogger(this.javaClass)
         logger.info("Got new request to handle.")
@@ -47,11 +49,11 @@ class Compiler {
             }
 
             logger.info("Finished handling request.")
-            ResponseEntity.status(HttpStatus.OK).body(makeMap("output", runOutput))
+            ResponseEntity.status(HttpStatus.OK).body(makeMap("output", "$buildError\nProgram output:\n$runOutput"))
         }
         catch (e: Exception) {
             logger.warn("Failed with message '${e.message}'.")
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(makeMap("error", e.message ?: ""))
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(makeMap("output", e.message ?: ""))
         }
     }
 
